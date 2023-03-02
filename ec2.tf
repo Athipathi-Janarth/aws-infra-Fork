62743ecac50d4da10d80ac791ce254d3dec603be
Athipathi-Janarth <janarth.a@northeastern.edu>
2023-03-02 02:59:58 -0500

    Added RDS

--- ec2.tf
+++ ec2.tf
@@ -1,15 +1,25 @@
 resource "aws_instance" "ec2_instance" {
-  ami                    = var.AMI_ID
-  instance_type          = "t2.micro"
-  key_name               = "ec2"
-  subnet_id              = aws_subnet.public_subnet[0].id
-  vpc_security_group_ids = [aws_security_group.application.id]
-
+  ami                     = var.AMI_ID
+  instance_type           = "t2.micro"
+  key_name                = "ec2"
+  subnet_id               = aws_subnet.public_subnet[0].id
+  iam_instance_profile    = aws_iam_instance_profile.ec2_profile.name
+  vpc_security_group_ids  = [aws_security_group.application.id]
+  disable_api_termination = false
   root_block_device {
     volume_size = 50
     volume_type = "gp2"
   }
-
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo echo "DB_NAME=${var.DB_NAME}" >> /home/ec2-user/webapp/.env
+    sudo echo "DB_PASSWORD=${var.DB_PASSWORD}" >> /home/ec2-user/webapp/.env
+    sudo echo "DB_PORT=${var.RDS_PORT}" >> /home/ec2-user/webapp/.env
+    sudo echo "DB_USER_NAME=${var.DB_USER}" >> /home/ec2-user/webapp/.env
+    sudo echo "DB_HOST=${aws_db_instance.rds_instance.address}" >> /home/ec2-user/webapp/.env
+    sudo echo "AWS_BUCKET_NAME=${aws_s3_bucket.bucket.bucket}" >> /home/ec2-user/webapp/.env
+    sudo echo "AWS_BUCKET_REGION=${var.AWS_REGION}" >> /home/ec2-user/webapp/.env
+    EOF
   tags = {
     Name = var.EC2_NAME
   }
