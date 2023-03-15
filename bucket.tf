08769da3730c3df0274f36734450225c8b750313
Athipathi-Janarth <janarth.a@northeastern.edu>
2023-03-14 23:36:59 -0400

    created route 53

--- bucket.tf
+++ bucket.tf
@@ -18,8 +18,11 @@ resource "aws_s3_bucket_lifecycle_configuration" "lifecycle" {
   }
 }
 resource "aws_s3_bucket_public_access_block" "s3_public_access_block" {
-  bucket            = aws_s3_bucket.bucket.id
-  block_public_acls = true
+  bucket                  = aws_s3_bucket.bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "encryption" {
