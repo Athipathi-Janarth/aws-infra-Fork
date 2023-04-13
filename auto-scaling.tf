e75035cdbff37aa6c87abd2a7069ed5b3a42f656
Athipathi-Janarth <janarth.a@northeastern.edu>
2023-04-12 21:25:25 -0400

    added encryptions & certificates

--- auto-scaling.tf
+++ auto-scaling.tf
@@ -1,9 +1,29 @@
-resource "aws_launch_configuration" "asg_launch_config" {
-  image_id                    = var.AMI_ID
-  instance_type               = "t2.micro"
-  key_name                    = "ec2"
-  associate_public_ip_address = true
-  user_data                   = <<-EOF
+# resource "aws_launch_configuration" "asg_launch_config" {
+#   image_id                    = var.AMI_ID
+#   instance_type               = "t2.micro"
+#   key_name                    = "ec2"
+#   associate_public_ip_address = true
+#   user_data                   = <<-EOF
+#     #!/bin/bash
+#     sudo echo "DB_NAME=${var.DB_NAME}" >> /home/ec2-user/webapp/.env
+#     sudo echo "DB_PASSWORD=${var.DB_PASSWORD}" >> /home/ec2-user/webapp/.env
+#     sudo echo "DB_PORT=${var.RDS_PORT}" >> /home/ec2-user/webapp/.env
+#     sudo echo "DB_USER_NAME=${var.DB_USER}" >> /home/ec2-user/webapp/.env
+#     sudo echo "DB_HOST=${aws_db_instance.rds_instance.address}" >> /home/ec2-user/webapp/.env
+#     sudo echo "AWS_BUCKET_NAME=${aws_s3_bucket.bucket.bucket}" >> /home/ec2-user/webapp/.env
+#     sudo echo "AWS_BUCKET_REGION=${var.AWS_REGION}" >> /home/ec2-user/webapp/.env
+#     sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ec2-user/webapp/AmazonCloudWatchConfig.json -s
+#     EOF
+#   iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
+#   security_groups             = [aws_security_group.application.id]
+# }
+resource "aws_launch_template" "asg_launch_template" {
+  name                 = "my_launch_template"
+  image_id             = var.AMI_ID
+  instance_type        = "t2.micro"
+  key_name             = "ec2"
+  vpc_security_group_ids = [aws_security_group.application.id]
+  user_data            = base64encode(<<-EOF
     #!/bin/bash
     sudo echo "DB_NAME=${var.DB_NAME}" >> /home/ec2-user/webapp/.env
     sudo echo "DB_PASSWORD=${var.DB_PASSWORD}" >> /home/ec2-user/webapp/.env
@@ -14,20 +34,35 @@ resource "aws_launch_configuration" "asg_launch_config" {
     sudo echo "AWS_BUCKET_REGION=${var.AWS_REGION}" >> /home/ec2-user/webapp/.env
     sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ec2-user/webapp/AmazonCloudWatchConfig.json -s
     EOF
-  iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
-  security_groups             = [aws_security_group.application.id]
+    )
+    block_device_mappings {
+    device_name = "/dev/sdf"
+
+    ebs {
+      volume_size           = 50
+      volume_type           = "gp2"
+      delete_on_termination = true
+      encrypted             = true
+      kms_key_id            = aws_kms_key.ec2_encryption.arn
+    }
+  }
+  iam_instance_profile {
+    name = aws_iam_instance_profile.ec2_profile.name
+  }
 }
 
-
 resource "aws_autoscaling_group" "application" {
-  name                 = "Application auto-scaling"
+  name                 = "Application-auto-scaling"
   default_cooldown     = 60
   vpc_zone_identifier  = [for subnet in aws_subnet.public_subnet : subnet.id]
-  launch_configuration = aws_launch_configuration.asg_launch_config.name
+  # launch_configuration = aws_launch_configuration.asg_launch_config.name
   min_size             = 1
   max_size             = 3
   desired_capacity     = 1
-
+  launch_template {
+      id      = aws_launch_template.asg_launch_template.id
+      version = "$Latest"
+  }
   tag {
     key                 = "ec2"
     value               = "webapp-instance"
--- prod-variables.tfvars
+++ prod-variables.tfvars
@@ -14,7 +14,7 @@ GATEWAY_NAME        = "InternetGateway"
 PUBLIC_ROUTE_TABLE  = "public_route_table"
 PRIVATE_ROUTE_TABLE = "private_route_table"
 
-AMI_ID      = "ami-084e586bd1080a04c"
+AMI_ID      = "ami-0a5f7bf366a026503"
 EC2_NAME    = "ec2"
 RDS_NAME    = "rds"
 RDS_PORT    = 3306
@@ -24,3 +24,4 @@ DB_PASSWORD = ""
 DB_USER     = "csye6225"
 HOSTED_ZONE = "Z08305211CYGIUMXRXNDU"
 APP_PORT    = 5002
+CERTIFICATE = "arn:aws:acm:us-east-1:052936583614:certificate/4c342558-e333-44cb-b8a6-fa4d9d870cb3"
